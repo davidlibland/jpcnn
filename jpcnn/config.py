1702230f1fb2d4340c5996b752218593612977a4
David Li-Bland <david.libland@gmail.com>
2018-10-03 13:20:54 -0700

    Save configurations and global_step

---
+++ jpcnn/config.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class JPCNNConfig:
+    image_dim: int
+    num_layers: int=2
+    num_resnet: int=2
+    num_filters: int=64
+    lr: float=1e-2
+    epochs: int=500
+    description: str="tmp"
+    ckpt_interval: int=1
--- jpcnn/train.py
+++ jpcnn/train.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from jpcnn.config import JPCNNConfig
 from jpcnn.data import get_dataset
 from jpcnn.file_utils import (
     build_checkpoint_file_name,
@@ -8,21 +9,8 @@ from jpcnn.image_utils import display_images
 from jpcnn.model import model, pixel_cnn_loss
 import time
 import numpy as np
-from dataclasses import dataclass
 
 tf.enable_eager_execution()
-
-
-@dataclass
-class JPCNNConfig:
-    image_dim: int
-    num_layers: int=2
-    num_resnet: int=2
-    num_filters: int=64
-    lr: float=1e-2
-    epochs: int=500
-    description: str="tmp"
-    ckpt_interval: int=1
 
 
 def generate_and_save_images(model, epoch, test_input, container):
@@ -46,7 +34,7 @@ def train(dataset, conf: JPCNNConfig, ckpt_file: str=None):
     optimizer = tf.train.AdamOptimizer(conf.lr)
     container = tf.contrib.eager.EagerVariableStore()
 
-    conf, dir_name, start_epoch = load_or_save_conf(ckpt_file, conf)
+    conf, dir_name = load_or_save_conf(ckpt_file, conf)
     # Data dependent initialization:
     for i, images in enumerate(dataset):
         with container.as_default():
@@ -55,13 +43,17 @@ def train(dataset, conf: JPCNNConfig, ckpt_file: str=None):
                   num_filters = conf.num_filters, num_resnet = conf.num_resnet,
                   init = True)
 
+    global_step = tf.train.get_or_create_global_step()
     saver = tf.train.Saver(
-        var_list = container.trainable_variables(),
-        save_relative_paths = True
+        var_list = container.trainable_variables()+[global_step],
+        save_relative_paths = True,
+        sharded = False
     )
     if ckpt_file is not None:
         saver.restore(None, ckpt_file)
-    for epoch in range(start_epoch, conf.epochs):
+    while global_step < conf.epochs:
+        print(global_step)
+        global_step.assign_add(1)
         start = time.time()
         total_loss = []
         for images in dataset:
@@ -86,27 +78,27 @@ def train(dataset, conf: JPCNNConfig, ckpt_file: str=None):
                 zip(gradients, container.trainable_variables())
             )
 
-        if epoch % conf.ckpt_interval == 0:
+        if int(global_step) % conf.ckpt_interval == 0:
             # display.clear_output(wait=True)
             generate_and_save_images(
                 lambda pred: model(pred, num_layers=conf.num_layers,
                                    num_filters=conf.num_filters,
                                    num_resnet=conf.num_resnet),
-                epoch + 1,
+                global_step,
                 noise,
                 container
             )
-            ckpt_name = build_checkpoint_file_name(dir_name, conf.description,
-                                                   epoch)
-            saver.save(None, ckpt_name)
+            ckpt_name = build_checkpoint_file_name(dir_name, conf.description)
+            fp = saver.save(None, ckpt_name, global_step=global_step)
+            print("Model Saved at {}".format(fp))
 
         # if (epoch + 1) % 15 == 0:
         #     checkpoint.save(file_prefix = checkpoint_prefix)
 
-        print('Time taken for epoch {} is {} sec'.format(epoch + 1,
+        print('Time taken for epoch {} is {} sec'.format(global_step,
                                                       time.time()-start))
 
-        print('Loss for epoch {} is {}'.format(epoch + 1, np.mean(total_loss)))
+        print('Loss for epoch {} is {}'.format(global_step, np.mean(total_loss)))
     # display.clear_output(wait = True)
     generate_and_save_images(
         lambda pred: model(pred, num_layers=conf.num_layers,
@@ -120,4 +112,4 @@ def train(dataset, conf: JPCNNConfig, ckpt_file: str=None):
 
 if __name__ == "__main__":
     train_dataset, image_dim = get_dataset(basic_test_data = True)
-    train(train_dataset, JPCNNConfig(image_dim=image_dim), ckpt_file = "Checkpoint-20180928-113325/params_tmp_5.ckpt")
+    train(train_dataset, JPCNNConfig(image_dim=image_dim), ckpt_file = "Checkpoint-20181003-125436/params_tmp.ckpt-2")
