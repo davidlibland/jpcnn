bug fixes and numerical stablity issues resolved

jpcnn/model.py
@@ -118,9 +118,9 @@ def model(inputs, num_filters, num_layers, num_resnet=1, dropout_p=0.9, training
         assert len(ul_list) == 0, "All ul layers should be connected."
         logits = nn.nin_layer(
             ul,
-            1
+            3
         )
-        return tf.sigmoid(logits)
+        return logits
 
 def pixel_cnn_loss(input, output):
     return tf.losses.sigmoid_cross_entropy(input, output)

jpcnn/nn.py
@@ -261,18 +261,27 @@ def discretized_mix_logistic_loss(x, l, mixture_sizes):
     num_logistics = xshape[-1] // 3  # 2 params for each logistic + 1 mix param
     logit_probs = x[...,:num_logistics]
     means = x[..., num_logistics: 2*num_logistics]
-    log_scales = x[..., num_logistics: 2*num_logistics]
+    log_scales = x[..., 2*num_logistics:]
     target_indices = [i for j, l in enumerate(mixture_sizes) for i in [j]*l]
     component_targets = tf.gather(l, target_indices, axis=-1)
     centered_targets = component_targets - means
     inv_scale = tf.exp(-log_scales)
     normalized_targets_m = (centered_targets - 0.5) * inv_scale
     normalized_targets_p = (centered_targets + 0.5) * inv_scale
-    cdf_m = tf.nn.sigmoid(normalized_targets_m)
-    cdf_p = tf.nn.sigmoid(normalized_targets_p)
-    target_prob = cdf_p - cdf_m  # Note: difference could be numerically unstable
 
-    mixture_log_probs = logit_probs + tf.log(target_prob)
+    # Note: mixture_log_loss & mixture_log_probs compute the following:
+    # cdf_m = tf.nn.sigmoid(normalized_targets_m)
+    # cdf_p = tf.nn.sigmoid(normalized_targets_p)
+    # target_prob = cdf_p - cdf_m  # Note: difference could be numerically unstable
+    # mixture_log_probs = logit_probs + tf.log(target_prob)
+
+    mixture_log_loss = sum([
+        log_expm1(inv_scale),
+        - tf.math.softplus(normalized_targets_p),
+        - tf.math.softplus(-normalized_targets_m)
+    ])
+
+    mixture_log_probs = logit_probs + mixture_log_loss
     stacked = tf.stack([mixture_log_probs, logit_probs], axis = -1)
     components = tf.split(stacked, mixture_sizes, axis=-2)
     mixed_probs = sum(tf.reduce_logsumexp(comp, axis=-2) for comp in components)
@@ -346,3 +355,10 @@ def partition_offsets(partition_sizes):
         tf.concat([[0] + partition_sizes[:-1]], axis = 0)
     ), dtype = tf.int32)
     return offsets
+
+@tf.custom_gradient
+def log_expm1(x):
+    """Numerically stable log(expm1(_))"""
+    def grad(dy):
+        return dy*tf.where(x > 0, -1./tf.expm1(-x), tf.exp(x)/tf.expm1(x))
+    return tf.where(x > 50, x+tf.log1p(-tf.exp(-x)), tf.log(tf.expm1(x))), grad

jpcnn/train.py
@@ -10,6 +10,11 @@ from jpcnn.model import model, pixel_cnn_loss
 import time
 import numpy as np
 
+from jpcnn.nn import (
+    discretized_mix_logistic_loss,
+    sample_from_discretized_mix_logistic,
+)
+
 tf.enable_eager_execution()
 
 
@@ -22,7 +27,7 @@ def generate_and_save_images(model, epoch, test_input, container, root_dir):
             with container.as_default():
                 ij_likelihood = model(predictions)[:, j, i, :]
             # print(ij_likelihood.mean(), ij_likelihood.std())
-            ij_sample = np.random.binomial(1, ij_likelihood)
+            ij_sample = sample_from_discretized_mix_logistic(ij_likelihood, [1])
             predictions[:,j,i,:] = ij_sample
 
     display_images(root_dir, 'image_at_epoch_{:04d}.png'.format(epoch), predictions[:,:,:,0])
@@ -63,12 +68,12 @@ def train(dataset, conf: JPCNNConfig, ckpt_file: str=None):
             with tf.GradientTape() as gr_tape, container.as_default():
                 image_var = tf.contrib.eager.Variable(images)
 
-                generated_images = model(image_var, training = True,
+                logits = model(image_var, training = True,
                                          num_layers=conf.num_layers,
                                          num_filters=conf.num_filters,
                                          num_resnet=conf.num_resnet)
 
-                loss = pixel_cnn_loss(images, generated_images)
+                loss = tf.reduce_mean(discretized_mix_logistic_loss(logits, images, [1]))
             with tf.contrib.summary.always_record_summaries():
                 tf.contrib.summary.scalar("loss", loss)
             total_loss.append(np.array(loss))