35c8da974521cb1f82b24240d1d3165bba9036cd
David Li-Bland <david.libland@gmail.com>
2019-02-25 13:31:19 -0800

    Changed config

--- jpcnn/config.py
+++ jpcnn/config.py
@@ -6,8 +6,8 @@ from typing import List
 class JPCNNConfig:
     image_dim: int
     num_layers: int=2
-    num_resnet: int=2
-    num_filters: int=32
+    num_resnet: int=5
+    num_filters: int=128
     lr: float=1e-3
     epochs: int=500
     description: str="mnist"
