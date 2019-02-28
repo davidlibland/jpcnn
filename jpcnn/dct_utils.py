c51e17a7dbee168918767f9474db116761b5241a
David Li-Bland <david.libland@gmail.com>
2019-02-27 20:52:24 -0800

    compression now orders frequencies lowest to highest

--- jpcnn/dct_utils.py
+++ jpcnn/dct_utils.py
@@ -1,6 +1,7 @@
 from typing import List
 
 import tensorflow as tf
+import numpy as np
 
 from jpcnn.nn import get_shape_as_list
 
@@ -119,15 +120,28 @@ def jpeg_reconstruction(x, compression):
 def flat_compress(x, compression):
     strides = get_shape_as_list(compression)
     compressed = jpeg_compression(x, strides, compression)
-    in_shape = get_shape_as_list(compressed)
-    return tf.reshape(compressed, in_shape[:3]+[-1])
+    compressed_p = tf.transpose(compressed, perm = [0, 1, 2, 5, 3, 4])
+    in_shape = get_shape_as_list(compressed_p)
+    freq_ordered = diagonal_flatten(compressed_p)
+    # freq_ordered goes from lowest to highest frequencies in the last axis
+    freq_prioritized = tf.transpose(freq_ordered, perm = [0, 1, 2, 4, 3])
+    # freq_prioritized places the frquencies before the channels prior to
+    # flattening; thus we cycle through channels faster than frequencies
+    # in the flattened array.
+    return tf.reshape(freq_prioritized, in_shape[:3]+[-1])
 
 
 def flat_reconstruct(x, compression):
     strides = get_shape_as_list(compression)
+    compressed_length = strides[0] * strides[1]
     in_shape = get_shape_as_list(x)
-    reshaped = tf.reshape(x, in_shape[:3] + strides + [-1])
-    return jpeg_reconstruction(reshaped, compression)
+    channels = in_shape[-1]//compressed_length
+    pre_compressed_shape = in_shape[:3]+[channels]+strides
+    reshaped = tf.reshape(x, in_shape[:3] + [compressed_length, channels])
+    freq_deprioritized = tf.transpose(reshaped, perm = [0, 1, 2, 4, 3])
+    unflattened = undiagonal_flatten(freq_deprioritized, y_shape = pre_compressed_shape)
+    unflattened_p = tf.transpose(unflattened, perm = [0, 1, 2, 4, 5, 3])
+    return jpeg_reconstruction(unflattened_p, compression)
 
 
 def basic_compression(
@@ -148,3 +162,74 @@ def basic_compression(
         for j in range(patch_size[1]):
             compression[-1].append(lin_transformation(i+j))
     return compression
+
+
+def diagonal_flatten(x):
+    """
+    Traverses the last two axes diagonally from lower left to upper right,
+    as in the following ordering:
+    [[0, 1, 3],
+     [2, 4, 6],
+     [5, 7, 8]]
+    """
+    x_shape = get_shape_as_list(x)
+    _, perm = get_diagonal_flatten_permutations(x_shape)
+    flat_x = tf.reshape(x, x_shape[:-2] + [-1])
+    return tf.gather(flat_x, indices = perm, axis = -1)
+
+
+def undiagonal_flatten(x, y_shape=None):
+    """
+    Traverses the last two axes diagonally from lower left to upper right,
+    as in the following ordering:
+    [[0, 1, 3],
+     [2, 4, 6],
+     [5, 7, 8]]
+    """
+    x_shape = get_shape_as_list(x)
+    if y_shape is None:
+        sqrt = int(np.sqrt(x_shape[-1]))
+        y_shape = x_shape[:-1] + [sqrt, sqrt]
+    perm, _ = get_diagonal_flatten_permutations(y_shape)
+    unperm_x = tf.gather(x, indices = perm, axis = -1)
+    return tf.reshape(unperm_x, y_shape)
+
+
+def get_diagonal_flatten_index_maps(x_shape):
+    weights = dict()
+    for i in range(x_shape[-2]):
+        for j in range(x_shape[-1]):
+            n = i + j  # this is the "weight" of the diagonal.
+            m = n * (n + 1) / 2  # The number of subdiagonal elements.
+            # This is the weight assigned to the i,j-th entry of
+            # an infinite matrix:
+            weights[(i, j)] = m + j
+    # Viewing our matrix as a subset of the infinite matrix gives the correct
+    # ordering, so sort according to it, and then take the final position as
+    # the index:
+    index_mapping = sorted(weights.keys(), key = weights.get)
+    mapping = {
+        (i, j): k
+        for k, (i, j) in enumerate(index_mapping)
+    }
+    inverse_mapping = {
+        k: (i, j)
+        for k, (i, j) in enumerate(index_mapping)
+    }
+    return mapping, inverse_mapping
+
+
+def get_diagonal_flatten_permutations(x_shape):
+    mapping, inverse_mapping = get_diagonal_flatten_index_maps(x_shape)
+    def flattened_index(i,j):
+        return i * x_shape[-1] + j
+    forward_perm = {
+        flattened_index(i, j): k
+        for (i, j), k in mapping.items()
+    }
+    reverse_perm = {
+        k: flattened_index(i, j)
+        for (i, j), k in mapping.items()
+    }
+    return [forward_perm[i] for i in range(len(forward_perm))], \
+           [reverse_perm[i] for i in range(len(reverse_perm))]
