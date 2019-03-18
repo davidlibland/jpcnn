fixed scaling bug in sampling, centered data

jpcnn/data.py
@@ -49,19 +49,17 @@ def get_dataset(batch_size = BATCH_SIZE, image_processors=None, basic_test_data 
     else:
         (all_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
         all_images = all_images.reshape(all_images.shape[0], 28, 28,
-                                            1).astype(dtype)  # [train_labels==0,:,:,:]
-        # We are normalizing the images to the range of [0, 1]
-        # train_images = np.round(train_images / 256).astype(dtype)
+                                            1).astype(dtype)
         all_images = all_images.astype(dtype)
     buffer_size = all_images.shape[0]
     assert all_images.shape[1] == all_images.shape[2], "Images should be square"
     image_dim = all_images.shape[1]
-    # Normalize images to the range of [0, 1]
+    # Normalize images to the range of [-1, 1]
     image_max = tf.reduce_max(all_images)
     image_min = tf.reduce_min(all_images)
     assert image_max > image_min, "Must have nontrivial images"
-    if image_max > 1 or image_min < 0:
-        all_images = (all_images - image_min)/(image_max - image_min)
+    if image_max > 1 or image_min < -1:
+        all_images = 2*(all_images - image_min)/(image_max - image_min) - 1
     print("Buffer size: %d" % buffer_size)
     print("Mini Batch size: %d" % batch_size)
     print("Image size: %d" % image_dim)

jpcnn/nn.py
@@ -565,8 +565,8 @@ def sample_from_discretized_mix_logistic(x, mixture_sizes):
         params = x[..., 1::3],
         indices = softmax_indices,
     )
-    logistic_scales = tf.exp(tf.batch_gather(
-        params = x[..., 2::3],
+    logistic_scales = 1/tf.nn.softplus(tf.batch_gather(
+        params = -x[..., 2::3],
         indices = softmax_indices,
     ))
     u = tf.random_uniform(logistic_means.get_shape(),

jpcnn/train.py
@@ -55,7 +55,7 @@ def generate_and_save_images(model, epoch, test_input, container, root_dir, comp
 
                 # crop values to [0,1] interval:
                 reconstruction = flat_reconstruct(predictions, compression)
-                capped_sample = tf.maximum(tf.minimum(reconstruction, 1), 0)
+                capped_sample = tf.maximum(tf.minimum(reconstruction, 1), -1)
                 recompression = flat_compress(capped_sample, compression)
                 chan_start = k * chan_per_freq
                 cap_adjustments.append(float(tf.reduce_max(